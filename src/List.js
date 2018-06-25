194226a4a72e4f03bb008847a337dcc8846192cc
Lanette Creamer <2926066+lanettecream@users.noreply.github.com>
2018-06-25 13:44:57 -0700

    Recovering files that were overwritten

---
+++ src/List.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+import './List.css';
+import { Link } from 'react-router-dom';
+import print from './print.svg';
+
+
+class List extends Component {
+
+
+    render() {
+
+    var savedIdeas = this.props.savedIdeas;
+    var removeListCallback = this.props.removeListIdea;
+    var resetIdeaCallback = this.props.resetIdea;
+
+ 
+    console.log("Saved Ideas: " + savedIdeas);
+
+    return ( 
+        <div className="Idea Section">
+
+            <div className="Idea">
+             <div className="savedhead"><Link to={{ pathname: "/checklist", state: { savedIdeas: savedIdeas } }}><img src={print} />
+                </Link>&nbsp;&nbsp;Saved Test Ideas&nbsp;<button className="nxtbtn" onClick={() => resetIdeaCallback()}>Reset</button>
+                <ul className="idealist">{savedIdeas.map(function(idea, index) {
+                    console.log("Idea: " + JSON.stringify(idea));
+                    return (<li key={index}>{idea.idea}&nbsp;&nbsp;<button className="removebtn" onClick={() => removeListCallback(idea)}>x</button><br /><br /></li>);
+                })}
+                </ul>
+            </div>
+        </div>
+    </div>
+    );
+    } 
+}
+
+
+/* List.propTypes = {
+    removeListCallback: PropTypes.func,
+    printListCallback: PropTypes.func,
+    ideas: PropTypes.array
+};
+
+List.defaultProps = {
+    idea: PropTypes.any,
+    index: PropTypes.number
+}; */
+
+export default List;
